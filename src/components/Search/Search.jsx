a411698f82d195cd88ebe61a0d1d2f121ed5e819
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2024-01-17 18:05:33 +0300

    feat: закончил Урок#1

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,5 @@
+// import style from './Header.module.scss';
+
+export const Search = () => {
+  return <div>Search</div>;
+};
---
+++ src/views/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import { Container } from '../Container/Container';
+import style from './Footer.module.scss';
+
+export const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <Container>footer</Container>
+    </footer>
+  );
+};
