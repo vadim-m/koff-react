35c3dca4185063bea4df28ecd6aa86d2ff1cad7d
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2024-01-25 23:15:35 +0300

    test: специально ломаю проверку линтера в App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Header } from './views/Header/Header';
 import { Main } from './views/Main/Main';
 
 export const App = () => {
+  var zero = 0;
+  
   return (
     <>
       <Header></Header>
