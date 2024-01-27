439f38e5acf0db5d9a087d402e4b7dcf0180c7be
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2024-01-27 14:25:14 +0300

    feat: добавил auth reducer

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const TOKEN_URL = 'https://koff-api.vercel.app/api/users/accessKey';
+export const CATEGORIES_URL = 'https://koff-api.vercel.app/api/productCategories';
+export const PRODUCTS_URL = 'https://koff-api.vercel.app/api/products';
--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,8 @@
-// redux
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth/auth.slice.js';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
