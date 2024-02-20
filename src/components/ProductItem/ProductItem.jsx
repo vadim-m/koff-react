0629b7604ea1a033dd5a1eb55ff4a492cbd52d4f
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2024-02-20 13:38:36 +0300

    feat: создал отдельный компонент для таблиц Table

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -3,6 +3,7 @@ import { ProductSlider } from '../ProductSlider/ProductSlider';
 import { Button } from '../Button/Button';
 import { LikeButton } from '../LikeButton/LikeButton';
 import style from './ProductItem.module.scss';
+import { Table } from '../Table/Table';
 
 export const ProductItem = ({ data, error }) => {
   if (error) {
@@ -12,39 +13,24 @@ export const ProductItem = ({ data, error }) => {
       </Container>
     );
   }
-
-  const { name, price, article } = data;
+  console.log(data);
+  const { id, name, price, article, characteristics, images } = data;
 
   return (
     <Container className={style.container}>
       <section className={style.card}>
         <h2 className={style.title}>{name}</h2>
-        <ProductSlider />
+        <ProductSlider images={images} />
         <div className={style.info}>
           <p className={style.price}>{(+price).toLocaleString('ru-RU')} ₽</p>
           <p className={style.article}>арт. {article}</p>
           <div className={style.characteristics}>
             <h3 className={style.characteristicsTitle}>Общие характеристики</h3>
-            <table className={style.table}>
-              <tbody>
-                <tr className={style.row}>
-                  <th className={style.key}>Dennis</th>
-                  <td className={style.value}>Web accessibility</td>
-                </tr>
-                <tr className={style.row}>
-                  <th className={style.key}>Sarah</th>
-                  <td className={style.value}>JavaScript frameworks</td>
-                </tr>
-                <tr className={style.row}>
-                  <th className={style.key}>Karen</th>
-                  <td className={style.value}>Web performance</td>
-                </tr>
-              </tbody>
-            </table>
+            <Table data={characteristics} />
           </div>
           <div className={style.btns}>
             <Button text="В корзину" filled />
-            <LikeButton />
+            <LikeButton id={id} />
           </div>
         </div>
       </section>
