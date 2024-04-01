ae6380f1274598174a4fd76977cc0a4acff1ec14
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2024-04-01 21:23:46 +0300

    feat: добавил на клопки слайдера <- -> :disabled состояние

--- src/components/ProductSlider/ProductSlider.jsx
+++ src/components/ProductSlider/ProductSlider.jsx
@@ -8,6 +8,22 @@ import 'swiper/css';
 export const ProductSlider = ({ images, name }) => {
   const [mainSwiper, setMainSwiper] = useState(null);
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+
+  const onProgress = () => {
+    if (!mainSwiper) return;
+    if (mainSwiper.isEnd) {
+      setIsEnd(true);
+      setIsBeginning(false);
+    } else if (mainSwiper.isBeginning) {
+      setIsBeginning(true);
+      setIsEnd(false);
+    } else {
+      setIsEnd(false);
+      setIsBeginning(false);
+    }
+  };
 
   return (
     <div className={style.picture}>
@@ -16,6 +32,7 @@ export const ProductSlider = ({ images, name }) => {
           modules={[Thumbs]}
           thumbs={{ swiper: thumbsSwiper }}
           onSwiper={setMainSwiper}
+          onProgress={onProgress}
           spaceBetween={20}
           slidesPerView={'auto'}
         >
@@ -28,12 +45,14 @@ export const ProductSlider = ({ images, name }) => {
               </SwiperSlide>
             ))}
           <button
+            disabled={isBeginning}
             className={`${style.arrow} ${style.arrow_prev}`}
             onClick={() => mainSwiper.slidePrev()}
           >
             &#60;
           </button>
           <button
+            disabled={isEnd}
             className={`${style.arrow} ${style.arrow_next}`}
             onClick={() => mainSwiper.slideNext()}
           >
